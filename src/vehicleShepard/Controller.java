8393ff8d63f2cf0fb06f1ab181de7c3991f074d4
Phillip Phoelich <ppho@itu.dk>
2011-12-09 12:15:42 +0100

    Added some functions to the controller for the user classes

--- src/vehicleShepard/Controller.java
+++ src/vehicleShepard/Controller.java
@@ -1,5 +1,7 @@
 package vehicleShepard;
 
+import java.util.ArrayList;
+
 public class Controller {
 	
 	public static void main(String[] args) {
@@ -8,30 +10,73 @@ public class Controller {
 	
 	//The Controller
 	private DesktopView view;	//The main view object
+	public static final int DAY = 0;
+	public static final int MONTH = 1;
+	public static final int YEAR = 2;
 	
 	public Controller()
 	{
 		view = new DesktopView(this);
 	}
 	
+	////////
+	//USER//
+	////////
+	
 	//CUSTOMER
-	public User getUser()
+	public Object[] getCustomer(int userID)
 	{
-		User user = null;
-		Customer cust = new Customer();
-		Mechanic mech = new Mechanic();
+		CustomerDB cust = new CustomerDB();
 		
-		user = cust.getUser();
-		if (!(user != null)) {
-			user = mech.getUser();
-		}
-		
-		return user;
+		return cust.getUserByID(userID);
 	}
 	
-	public String[][] getList()
+	public Object[][] getCustomerList()
 	{
+		CustomerDB cust = new CustomerDB();
 		
+		return cust.getList();
 	}
+	
+	public void newCustomer()
+	{
+		CustomerDB cust = new CustomerDB();
+		
+		cust.newUser(); //TODO Add parameters, when Lauge has added them to the signature
+	}
+	
+	//MECHANIC
+	public Object[] getMechanic(int userID)
+	{
+		MechanicDB mech = new MechanicDB();
+		
+		return mech.getUserByID(userID);
+	}
+	
+	public Object[][] getMechanicList()
+	{
+		MechanicDB mech = new MechanicDB();
+		
+		return mech.getList();
+	}
+	
+	public void newMechanic()
+	{
+		MechanicDB mech = new MechanicDB();
+		
+		mech.newUser(); //TODO Add parameters, when Lauge has added them to the signature
+	}
+	
+	////////////////
+	//RESERVATIONS//
+	////////////////
+	
+	public ArrayList<Reservation> getReservationList()
+	{
+		ReservationDB reservations = new ReservationDB;
+		
+		return reservations.getArrayList();
+	}
+	
 	
 }
